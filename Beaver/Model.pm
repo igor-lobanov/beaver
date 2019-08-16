9efa6644a195232a048d94a85d6286d8f5feba9e
Igor Lobanov <igor.lobanov@gmail.com>
2019-08-17 00:03:19 +0300

    Add some POD

--- Beaver/Model.pm
+++ Beaver/Model.pm
@@ -46,3 +46,86 @@ sub vocs {
 }
 
 1;
+
+=pod
+
+=encoding utf8
+
+=head1 SYNOPSIS
+
+    use Mojo::Base 'Beaver::Model';
+
+    sub init {
+        my $m = shift;
+        # Model init code
+        $m;
+    }
+
+    my $item = $m->item;
+
+    my $items = $m->list;
+
+    say $_ for $m->columns;
+
+=head1 DESCRIPTION
+
+L<Beaver::Model> is base class for Beaver low-level models.
+
+=head1 ATTRIBUTES
+
+=head2 backend
+
+Reference to backend config section in config
+
+=head2 app
+
+Reference to application object. Models can use it to access application helpers for example.
+
+=head2 entity
+
+Name of entity which model represents.
+
+=head2 definition
+
+Data which describes model structure.
+
+=head2 c
+
+Reference to controller instance. Models can use it to access request data for example.
+
+=head1 METHODS
+
+=head2 init
+
+Init code which is executed after creating model object instance. Redefined in child classes.
+
+=head2 item
+
+Prototype method for high-level method. B<item> must return entity item and other supplimentary item.
+
+=head2 list
+
+Prototype method for high-level method. B<list> must return structure with entity items collection and other supplimentary data.
+
+=head2 fields
+
+Returns single field descriptive structure or array of field descriptive structures.
+
+=head2 field
+
+Synonym for fields.
+
+=head2 columns
+
+Returns list of model fields.
+
+=head2 vocs
+
+Returns list of model fields which are vocabulary references.
+
+=head1 SEE ALSO
+
+L<Mojolicious>, L<Mojolicious::Guides>, L<https://mojolicious.org>.
+
+=cut
+
